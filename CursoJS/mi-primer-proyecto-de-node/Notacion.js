fac309751fc6cc780259103cb7026753a1e0e614
Martin Andreani <tinchoandreani12@gmail.com>
2023-02-23 16:23:35 -0300

    Segunda Parte

---
+++ CursoJS/mi-primer-proyecto-de-node/Notacion.js
@@ -0,0 +1,11 @@
+//Notacion
+
+// ; --> Denotar eñ final de linea
+
+// . --> Se utiliza en los objetos para acceder a los atributos
+
+// [] --> Listas, arreglos o arrays
+
+// {} --> Llaves para objetos, funciones y estructuras de control
+
+// () --> Funciones
---
+++ CursoJS/mi-primer-proyecto-de-node/Segundo Ejercicio.js
@@ -0,0 +1,20 @@
+// Segundo ejercicio
+
+
+const nom = "Martin Andreani";
+const edad= 19;
+const des= true;
+const nac = new Date(2003,2,4);
+const libro ={
+    tit: "Harry Potter - Las Reliquias de la Muerte",
+    autor: "J. K. Rowling",
+    fec: new Date( 2007,7,21),
+    url: "https://articulo.mercadolibre.com.ar/MLA-1138454992-harry-potter-7-las-reliquias-de-la-muerte-tapa-blanda--_JM#position=6&search_layout=stack&type=item&tracking_id=93994783-9812-4093-918a-69f686e68b02"
+}
+
+
+console.log(nom);
+console.log(edad);
+console.log(des);
+console.log(nac);
+console.log(libro);
