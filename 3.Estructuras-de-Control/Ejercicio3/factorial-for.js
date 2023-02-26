bd57642936bc0362a2a1be2e02fa67e2da25ab71
Martin Andreani <tinchoandreani12@gmail.com>
2023-02-26 14:16:12 -0300

    Parte 3

---
+++ 3.Estructuras-de-Control/Ejercicio3/factorial-for.js
@@ -0,0 +1,6 @@
+let fact =1
+for (i=1;i <= 10;i++ ){
+    fact = i*fact
+}
+
+console.log(`El factorial de 10 es : ${fact}`);
---
+++ 3.Estructuras-de-Control/Ejercicio3/factorial-while.js
@@ -0,0 +1,10 @@
+
+let cont= 0
+let fact  = 1
+while (cont < 10){
+    cont ++
+    fact = cont * fact
+    console.log(fact);
+
+}
+console.log(`El factorial de 10 es : ${fact}`);
